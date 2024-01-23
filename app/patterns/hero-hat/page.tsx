de31e9c691e5fc82d2b269400c815837e75e07a0
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2024-01-23 18:46:09 -0500

    FIX figured out how to make pages, hero hat page works

---
+++ app/patterns/hero-hat/page.tsx
@@ -0,0 +1,16 @@
+
+export default function HeroHat() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        
+        <div>
+            <h1>The Hat of a Hero</h1>
+            <h2>A FREE Testing Crochet Pattern</h2>
+        </div>
+        
+  
+        
+      </main>
+    )
+  }
+  
