7d682c2549af0dd9b90e760bff14e05f985f52e8
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2024-02-22 18:51:14 -0500

    ADD HeroSection.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <div className="main">{children}</div></body>
+        <main className="container">{children}
+        </main>
+        </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import Image from 'next/image'
+import HeroSection from './home/HeroSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-      <div>
-        <h1>Alyssa Dannielle Designs</h1>
-        <h2>Crochet Patterns by Alyssa Dannielle</h2>
-      </div>
-      
-    </main>
+    <> 
+      <HeroSection />
+    </>
   )
 }
