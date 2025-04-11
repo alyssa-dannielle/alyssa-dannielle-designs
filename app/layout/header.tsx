57e6d2373aeff07c0deabfa0421d3c993578be6a
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2025-04-11 14:45:13 -0400

    ADD home page link to header REMOVE signin button

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Header = (props: Props) => {
-    return (
-        <div className='border-b flex justify-between'>
-            <div className='text-4xl px-2 py-4'>LOGO</div>
-            <button className='text-white bg-black px-4 py-4'>Sign In</button>
-        </div>
-    )
-}
+  return (
+    <div className="border-b flex justify-between">
+      <div className="p-5">
+        <h1>
+          <a href="http://alyssadannielle.design"> AlyssaDannielle.Design </a>
+        </h1>
+      </div>
+      {/* <div className="text-4xl px-2 py-4">LOGO</div>
+      <button className="text-white bg-black px-4 py-4">Sign In</button> */}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
