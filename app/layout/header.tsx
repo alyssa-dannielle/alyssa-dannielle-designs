49c29d6107b674739b0ce7ea1ed50f4a4dd3c871
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2025-04-11 19:57:06 -0400

    UPDATE link in header

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 type Props = {};
 
@@ -6,9 +7,7 @@ const Header = (props: Props) => {
   return (
     <div className="border-b flex justify-between">
       <div className="p-5">
-        <h1>
-          <a href="http://alyssadannielle.design"> AlyssaDannielle.Design </a>
-        </h1>
+        <Link href="/">AlyssaDannielle.Design</Link>
       </div>
       {/* <div className="text-4xl px-2 py-4">LOGO</div>
       <button className="text-white bg-black px-4 py-4">Sign In</button> */}
