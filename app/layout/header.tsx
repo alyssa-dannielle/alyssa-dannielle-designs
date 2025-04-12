b15b3853f89c733766855a9c2efee8aad60f70b0
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2025-04-12 17:31:28 -0400

    ADD dark light toggle functionality

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun } from "@fortawesome/free-regular-svg-icons";
@@ -7,6 +8,25 @@ import { faMoon } from "@fortawesome/free-solid-svg-icons";
 type Props = {};
 
 const Header = (props: Props) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setDarkMode(savedTheme === "dark");
+    }
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", darkMode);
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    const newDarkMode = !darkMode;
+    setDarkMode(newDarkMode);
+    localStorage.setItem("theme", newDarkMode ? "dark" : "light");
+  };
+
   return (
     <div className="bg-white text-black dark:bg-gray-900 dark:text-white">
       <div className="border-b flex justify-between">
@@ -14,9 +34,9 @@ const Header = (props: Props) => {
           <Link href="/">AlyssaDannielle.Design</Link>
         </div>
         <span className="p-5">
-          <button>
+          <button onClick={toggleDarkMode}>
             <FontAwesomeIcon
-              icon={faMoon}
+              icon={darkMode ? faSun : faMoon}
               className="w-3.5 h-3.5 sm:w-5 sm:h-5"
             />
           </button>
