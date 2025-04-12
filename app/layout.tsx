46ba953801eaacd449cf3b1bce61bac08dba15fd
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2025-04-12 07:44:46 -0400

    UPDATE metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from './layout/header'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "./layout/header";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Alyssa Dannielle's Designs",
-  description: 'browse free crochet patterns, become a pattern tester, buy pattern pdfs, shop already made items',
-}
+  description: "Browse and test free crochet patterns!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main className="container">{children}
-        </main>
-        </body>
+        <main className="container">{children}</main>
+      </body>
     </html>
-  )
+  );
 }
