2ce52fd20d927d08dfea5f273fd8ad41be4eae38
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2025-04-14 14:48:43 -0400

    11 add favicon (#14)
    * ADD yarn-ball image
    
    * REMOVE yarn-ball icon bc of watermark
    
    * ADD crochet_yarn_ball image
    
    * ADD crochet_yarn_ball.ico
    
    * UPDATE favicon to crochet_yarn_ball
    
    * ADD icon info to metadata
    
    * REMOVE code for nonexistant icons in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Alyssa Dannielle's Designs",
   description: "Browse and test free crochet patterns!",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
