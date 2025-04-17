2f0a73606a59544aeb273b76d6f9a7357a78ec60
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2025-04-17 17:24:43 -0400

    npm run format

--- app/patterns/hero-sword/page.tsx
+++ app/patterns/hero-sword/page.tsx
@@ -1,21 +1,17 @@
 export default function HeroSword() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        
-        <div>
-            <h1>The Sword of a Hero</h1>
-            <h2>A FREE Testing Crochet Pattern</h2>
-            <p>This page may contain affiliate links - click here to read more</p>
-            <br />
-            <p>This is where i talk about the sword</p>
-            <br />
-            <h3>Materials: </h3> {/*add a hash link for each heading for skipping purposes*/}
-            <h3>Stitche & Abbreviations:</h3>
-        </div>
-        
-  
-        
-      </main>
-    )
-  }
-  
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <div>
+        <h1>The Sword of a Hero</h1>
+        <h2>A FREE Testing Crochet Pattern</h2>
+        <p>This page may contain affiliate links - click here to read more</p>
+        <br />
+        <p>This is where i talk about the sword</p>
+        <br />
+        <h3>Materials: </h3>{' '}
+        {/*add a hash link for each heading for skipping purposes*/}
+        <h3>Stitche & Abbreviations:</h3>
+      </div>
+    </main>
+  );
+}
