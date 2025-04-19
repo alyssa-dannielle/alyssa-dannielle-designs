eced7c76fbcaf7dbe530b24c80c93b5012990e68
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2025-04-19 14:43:16 -0400

    add netlify redirects config for pattern URLs

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/:pattern',
-        destination: '/patterns/:pattern',
-      },
-    ];
-  },
   output: 'export',
   images: {
     unoptimized: true,
