1eb522de67ac133099b871c39f032ac37db1187e
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2025-04-19 13:10:45 -0400

    adjust PatternStep styling

--- components/PatternStep.tsx
+++ components/PatternStep.tsx
@@ -6,10 +6,10 @@ interface PatternStepProps {
 export default function PatternStep({ text, stitchCount }: PatternStepProps) {
   return (
     <li>
-      <div className='flex items-center justify-between gap-4 py-1'>
+      <div className='flex flex-col sm:flex-row justify-between items-center'>
         <span className='flex-1'>{text}</span>
         {stitchCount && (
-          <span className='dark:text-gray-400 text-sm whitespace-nowrap'>
+          <span className='dark:text-gray-400 text-gray-600 italic self-end sm:ml-4'>
             ... {stitchCount} stitches
           </span>
         )}
