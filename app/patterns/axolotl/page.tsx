493292ab7e2db2033cb91f7c01418ccf72bdee4f
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2025-04-23 15:54:09 -0400

    copy imports from finished pattern

---
+++ app/patterns/axolotl/page.tsx
@@ -0,0 +1,9 @@
+import { GoogleAnalytics } from '@next/third-parties/google';
+import PatternLayout from '../../components/PatternLayout';
+import PatternSchema from '../../../components/PatternSchema';
+import PatternIntroduction from '../../components/PatternIntroduction';
+import PatternSupplyList from '../../components/PatternSupplyList';
+import PatternTechniquesList from '../../components/PatternTechniquesList';
+import PatternStepSection from '../../../components/PatternStepSection';
+import PatternStep from '../../../components/PatternStep';
+import PatternSectionHeading from '../../../components/PatternSectionHeading';
