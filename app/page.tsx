465ea48b115bf3edf012fdd7cd746703120a12c7
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2025-04-26 19:17:25 -0400

    update drink koozie to koozie (#52)
    * update drink koozie to koozie
    
    * updated koozie links

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <>
       <HeroSection />
       <div className='flex flex-col items-center gap-4 w-full mt-8 mb-8'>
-        <Link href='/patterns/drink-koozie'>
+        <Link href='/patterns/koozie'>
           <button className='px-6 py-3 rounded-lg bg-teal-700 text-white font-medium transform transition duration-200 ease-in-out hover:bg-teal-800 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-teal-600 focus:ring-offset-2'>
             View Drink Koozie Pattern
           </button>
