01f5d9aa3604ce6975b8bf39344cb98f56135522
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2025-05-07 14:13:57 -0400

    update button color

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,17 @@ export default function Home() {
       <HeroSection />
       <div className='flex flex-col items-center gap-4 w-full mt-8 mb-8'>
         <Link href='/patterns/koozie'>
-          <button className='px-6 py-3 rounded-lg bg-teal-700 text-white font-medium transform transition duration-200 ease-in-out hover:bg-teal-800 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-teal-600 focus:ring-offset-2'>
+          <button className='px-6 py-3 rounded-lg bg-teal-600 text-white font-medium transform transition duration-200 ease-in-out hover:bg-teal-700 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2'>
             Drink Koozie Pattern
           </button>
         </Link>
         <Link href='/patterns/pokeball'>
-          <button className='px-6 py-3 rounded-lg bg-teal-700 text-white font-medium transform transition duration-200 ease-in-out hover:bg-teal-800 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-teal-600 focus:ring-offset-2'>
+          <button className='px-6 py-3 rounded-lg bg-teal-600 text-white font-medium transform transition duration-200 ease-in-out hover:bg-teal-700 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2'>
             Pokéball Pattern
           </button>
         </Link>
         <Link href='/patterns/axolotl'>
-          <button className='px-6 py-3 rounded-lg bg-teal-700 text-white font-medium transform transition duration-200 ease-in-out hover:bg-teal-800 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-teal-600 focus:ring-offset-2'>
+          <button className='px-6 py-3 rounded-lg bg-teal-600 text-white font-medium transform transition duration-200 ease-in-out hover:bg-teal-700 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2'>
             Axolotl Pattern
           </button>
         </Link>
