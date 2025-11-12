b247080c5d5c1bceee5f64bb8c2acebda560842e
Alyssa Miller <alyssa.dannielle@gmail.com>
2025-11-11 23:00:05 -0500

    rearrange buttons and add space for kits

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,23 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      <div className='flex flex-col items-center gap-4 w-full mt-8 mb-8'>
-        <Button link='/patterns/koozie'> Drink Koozie Pattern</Button>
-        <Button link='/patterns/pokeball'>Pokéball Pattern</Button>
-        <Button link='/patterns/axolotl'>Axolotl Pattern</Button>
+      <div className='flex flex-col gap-4 mt-8 mb-8'>
+        <div className='flex flex-col items-center gap-4 w-full mt-8 mb-8'>
+          <h2 className='tracking-tight text-2xl sm:text-4xl md:text-6xl mt-8 text-center'>
+            Crochet Patterns
+          </h2>
+          <div className='flex sm:flex-col-1 md:flex-col-3 gap-4'>
+            <Button link='/patterns/koozie'> Drink Koozie</Button>
+            <Button link='/patterns/pokeball'>Pokéball</Button>
+            <Button link='/patterns/axolotl'>Axolotl</Button>
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-4 w-full mt-8 mb-8'>
+          <h2 className='text-2xl sm:text-4xl md:text-6xl'>Crochet Kits</h2>
+          <p>Drink Koozie</p>
+          <p>Pokeball</p>
+          <p>Axolotl</p>
+        </div>
       </div>
       <Footer />
     </>
