abf820cee0489b09c3c19ec79100de430430584e
Alyssa Miller <alyssa.dannielle@gmail.com>
2025-11-13 00:30:16 -0500

    UPDATE header to be sticky

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <HeroSection />
       <div className='flex flex-col gap-4 mt-8 mb-8'>
         <div className='flex flex-col items-center gap-4 w-full mt-8 mb-8'>
-          <h2 className='tracking-tight text-2xl sm:text-4xl md:text-6xl mt-8 text-center'>
+          <h2 className='tracking-tight text-2xl sm:text-4xl md:text-6xl text-center'>
             Crochet Patterns
           </h2>
           <div className='grid grid-cols-1 md:grid-cols-3 gap-4 place-items-center mt-8'>
