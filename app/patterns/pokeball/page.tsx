bea3ce646d59421deb183b68717b8840def98c5b
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2025-11-22 15:33:04 -0500

    Live pattern test updates (#68)
    * ADD braces to koozie pattern
    
    * UPDATE POKEBALL strands of to strands of yarn
    
    * UPDATE POKEBALL around to end to to the end
    
    * UPDATE POKEBALL white yarn to 28 grams AND remove or x yards

--- app/patterns/pokeball/page.tsx
+++ app/patterns/pokeball/page.tsx
@@ -33,9 +33,9 @@ export default function pokéball() {
 
   const supplies = [
     { text: 'Medium weight acrylic yarn' },
-    { text: 'Red - 24 grams or 46 yards', indent: true },
-    { text: 'White - 25 grams or 50 yards', indent: true },
-    { text: 'Black - 9 grams or 12 yards', indent: true },
+    { text: 'Red ~ 24 grams', indent: true },
+    { text: 'White ~ 28 grams', indent: true },
+    { text: 'Black ~ 9 grams', indent: true },
     { text: 'Polyfil ~30 grams' },
     { text: '5mm (H) crochet hook' },
     { text: 'Crochet notions:' },
@@ -107,7 +107,7 @@ export default function pokéball() {
             isCircular
           />
           <PatternStep
-            text='Using 2 strands of white, work 8 hdc in a magic ring, join'
+            text='Using 2 strands of white yarn, work 8 hdc in a magic ring, join'
             stitchCount='8'
           />
           <PatternStep
@@ -128,15 +128,15 @@ export default function pokéball() {
             isCircular
           />
           <PatternStep
-            text='Using 2 strands of red, work 8 hdc in a magic ring'
+            text='Using 2 strands of red yarn, work 8 hdc in a magic ring'
             stitchCount='8'
           />
           <PatternStep
-            text='Continuing around, work 1 hdcInc in each stitch around to end'
+            text='Continuing around, work 1 hdcInc in each stitch to the end'
             stitchCount='16'
           />
           <PatternStep
-            text='Repeat { 1 hdc in next stitch and 1 hdcInc in next stitch } around to end'
+            text='Repeat { 1 hdc in next stitch and 1 hdcInc in next stitch } to the end'
             stitchCount='24'
           />
           <PatternStep
@@ -144,11 +144,11 @@ export default function pokéball() {
             stitchCount='32'
           />
           <PatternStep
-            text='Repeat { 1 hdc in next 3 stitches and 1 hdcInc in next stitch } around to end'
+            text='Repeat { 1 hdc in next 3 stitches and 1 hdcInc in next stitch } to the end'
             stitchCount='40'
           />
           <PatternStep
-            text='Work 1 hdc in each stitch around to end, join'
+            text='Work 1 hdc in each stitch to the end, join'
             stitchCount='40'
           />
           <PatternStep
@@ -165,27 +165,27 @@ export default function pokéball() {
             isCircular
           />
           <PatternStep
-            text='Using 2 strands of white, work 8 hdc in a magic ring'
+            text='Using 2 strands of white yarn, work 8 hdc in a magic ring'
             stitchCount='8'
           />
           <PatternStep
-            text='Continuing around, work 1 hdcInc in each stitch around to end'
+            text='Continuing around, work 1 hdcInc in each stitch to the end'
             stitchCount='16'
           />
           <PatternStep
-            text='Repeat { 1 hdc in next stitch and 1 hdcInc in next stitch } around to end'
+            text='Repeat { 1 hdc in next stitch and 1 hdcInc in next stitch } to the end'
             stitchCount='24'
           />
           <PatternStep
-            text='Work 1 hdc in next stitch, repeat { 1 hdc in next 2 stitches and 1 hdcInc in next stitch } around to end, work 1 hdc in last stitch'
+            text='Work 1 hdc in next stitch, repeat { 1 hdc in next 2 stitches and 1 hdcInc in next stitch } to the end, work 1 hdc in second to last stitch and 1 hdcinc in last stitch'
             stitchCount='32'
           />
           <PatternStep
-            text='Repeat { 1 hdc in next 3 stitches and 1 hdcInc in next stitch } around to end'
+            text='Repeat { 1 hdc in next 3 stitches and 1 hdcInc in next stitch } to the end'
             stitchCount='40'
           />
           <PatternStep
-            text='Work 1 hdc in each stitch around to end, join'
+            text='Work 1 hdc in each stitch to the end, join'
             stitchCount='40'
           />
           <PatternStep
